40bdd71b4d4623ca9006ed70800ed05b8db22eff
Alex Movitz <alex.movitz1@gmail.com>
2026-07-24 02:56:33 -0600

    Adds loading the Cybiko app.

--- Pybiko.py
+++ Pybiko.py
@@ -3,8 +3,14 @@
 from .PybikoIO import PybikoIO
 from .PybikoSerial import PybikoSerial
 from time import sleep
+import subprocess
 
-class Pybiko():
+SRC_DIR = "/home/pi/src/"
+USB_PATH = SRC_DIR + "CybikoStuff/tools/build/usbcon"
+TUI_PATH = SRC_DIR + "CybikoTUI/CybikoTUI.boot"
+
+
+class Pybiko:
     def test(self):
         try:
             # Compile the Cybiko application
@@ -13,16 +19,33 @@ class Pybiko():
             PIO = PybikoIO()
             PIO.reset()
 
+            print(USB_PATH, "-b", TUI_PATH)
             # Load an application
+            result = subprocess.run(
+                [
+                    USB_PATH,
+                    "-b",
+                    TUI_PATH,
+                ],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+            )
+            print(result.stdout)
 
             # Connect to serial
             pserial = PybikoSerial()
+            retries = 10
             while not pserial.ping():
                 print("no response - check wiring/baud")
+                retries -= 1
+                if retries <= 0:
+                    raise IOError("Cannot connect to Cybiko")
                 sleep(1)
+            print("Cybiko connected!")
 
             pserial.clear()
-            pserial.set_cursor(2, 3)
+            pserial.set_cursor(5, 2)
             pserial.write_text("HELLO CYBIKO - LOVE PI")
         except Exception as e:
             print(e)
