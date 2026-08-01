1d26e46c46a10e906eb2cf587d87e4ebad9dfec5
Alex Movitz <alex.movitz1@gmail.com>
2026-07-31 18:11:47 -0600

    Sets terminal window size.

--- Pybiko.py
+++ Pybiko.py
@@ -13,16 +13,23 @@ import threading
 import queue
 import os
 import pty
+import fcntl
+import termios
+import struct
 
 SRC_DIR = "/home/pi/src/"
 USB_PATH = SRC_DIR + "CybikoStuff/tools/build/usbcon"
 TUI_PATH = SRC_DIR + "CybikoTUI/CybikoTUI.boot"
 
+def set_winsize(fd, rows, cols):
+    winsize = struct.pack("HHHH", rows, cols, 0, 0)
+    fcntl.ioctl(fd, termios.TIOCSWINSZ, winsize)
 
 class Pybiko:
     def __init__(self):
         try:
             # Compile the Cybiko application
+            sleep(1)
 
             # Connect to the Reset IO
             self.PIO = PybikoIO()
@@ -61,6 +68,12 @@ class Pybiko:
     def loop(self):
         try:
             master_fd, slave_fd = pty.openpty()
+            set_winsize(master_fd, rows=12, cols=26)
+
+            env = os.environ.copy()
+            env["TERM"] = "dumb"  # or "vt100" if you want to support cursor movement/escape codes
+            env["COLUMNS"] = "26"
+            env["LINES"] = "12"
 
             terminal = subprocess.Popen(
                 ["/bin/sh", "-i"],
@@ -68,6 +81,7 @@ class Pybiko:
                 stdout=slave_fd,
                 stderr=slave_fd,
                 preexec_fn=os.setsid,  # new session, slave becomes its controlling tty
+                env=env,
             )
             os.close(slave_fd)  # parent doesn't need this end anymore
 
